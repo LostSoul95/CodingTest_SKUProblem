f260fbbeccfad293441e0d716f15ecb6361c0b55
katyal.m <katyal.madhur@mahindra.com>
2020-06-29 01:28:25 +0530

    Test Suite Case 1 added

--- SKU_UnitTest/UnitTest1.cs
+++ SKU_UnitTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Coding_SKU.Models;
 using Xunit;
@@ -8,13 +9,43 @@ namespace SKU_UnitTest
     public class UnitTest1
     {
         [Fact]
-        public async Task TestSKU()
+        public async Task TestSKU1()
         {
-           // We will basically test the service layer by mocking objects from out test class
+            //Arrange
 
-           ProductDetails product =  new ProductDetails();
-           product.ProductType = "A";
-           product.ProductPrice = "50";
+            InterfaceSKU sku = new SKU();
+
+           // We will basically test the service layer by mocking objects from out test class
+           var products = new List<ProductDetails>();
+
+           ProductDetails productA =  new ProductDetails();
+           productA.ProductType = "A";
+           productA.ProductPrice = 50;
+           productA.ProductQuantity = 1;
+           products.Add(productA);
+
+            ProductDetails productB = new ProductDetails();
+           productB.ProductType = "B";
+           productB.ProductPrice = 30;
+           productB.ProductQuantity = 1;
+           products.Add(productB);
+
+            ProductDetails productC = new ProductDetails();
+           productC.ProductType = "B";
+           productC.ProductPrice = 20;
+           productC.ProductQuantity = 1;
+           products.Add(productC);
+
+           int total = 100;
+
+           // ACT
+
+           var response = sku.GetTotal(products);
+
+           // ASSERT 
+
+           Assert.True(response == total);
+            
 
         }
     }
