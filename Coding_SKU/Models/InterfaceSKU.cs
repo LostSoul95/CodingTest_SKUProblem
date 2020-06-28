f78794f4eea58f9d292b15869f279c52f7aef90e
katyal.m <katyal.madhur@mahindra.com>
2020-06-28 23:18:30 +0530

    ProductDetails Class and Method declaration in interface.

--- Coding_SKU/Models/InterfaceSKU.cs
+++ Coding_SKU/Models/InterfaceSKU.cs
@@ -6,6 +6,7 @@ namespace Coding_SKU.Models
 {
     public interface InterfaceSKU
     {
-
+        void GetPriceByType(ProductDetails product);
+        int GetTotalPrice(List<ProductDetails> products);
     }
 }
---
+++ Coding_SKU/Models/ProductDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coding_SKU.Models
+{
+     public  class  ProductDetails
+    {   
+        public string ProductType { get; set; }
+        public string ProductPrice { get; set; }
+
+    }
+}
