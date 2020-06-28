2feb04d57a5d8fe0d114e706ec4c5389f156ff82
katyal.m <katyal.madhur@mahindra.com>
2020-06-28 23:00:00 +0530

    Initial commit- Console app with unit test framework

---
+++ Coding_SKU/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coding_SKU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
